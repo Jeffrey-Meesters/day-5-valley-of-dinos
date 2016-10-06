11dea254df4b574838342e72d1732e993fbc52da
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-10-06 15:09:41 +0200

    added pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dinosaurs#index"
 
-  get "dinosaurs" => "dinosaurs#index"
+  get 'dinosaurs' => 'dinosaurs#index'
+  get 'dinosaurs/new' => "dinosaurs#new"
+  get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
+  get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
+    post "dinosaurs" => "dinosaurs#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-dinosaurs = Dinosaur.create( {name:"T-rex", age: 10, image_url: "https://pbs.twimg.com/profile_images/637098103359713281/_o9ctvhN.png" }),
-dinosaurs = Dinosaur.create( {name:"Diplosaurus", age: 8, image_url: "http://dl.myket.ir/imageresizing/icon/small/com.evolved.clan.of.dilophosaurus.png"}),
-dinosaurs = Dinosaur.create( {name:"Triceratops", age: 15, image_url: "http://static.spore.com/static/image/500/875/212/500875212841_lrg.png" })
+dinosaurs = Dinosaur.create( {name:"T-rex", age: rand(1..100).to_i, image_url: "https://pbs.twimg.com/profile_images/637098103359713281/_o9ctvhN.png" }),
+dinosaurs = Dinosaur.create( {name:"Diplosaurus", age: rand(1..100).to_i, image_url: "http://dl.myket.ir/imageresizing/icon/small/com.evolved.clan.of.dilophosaurus.png"}),
+dinosaurs = Dinosaur.create( {name:"Triceratops", age: rand(1..100).to_i, image_url: "http://static.spore.com/static/image/500/875/212/500875212841_lrg.png" })
