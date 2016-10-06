4722a230a963ce652a96b01a844ed29acbe3fe76
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-10-06 16:11:58 +0200

    added more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dinosaurs#index"
 
+  resources :dinosaurs
+
+=begin
   get 'dinosaurs' => 'dinosaurs#index'
   get 'dinosaurs/new' => "dinosaurs#new"
   get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
   get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
-    post "dinosaurs" => "dinosaurs#create"
+  post "dinosaurs" => "dinosaurs#create"
+=end
+
 end
